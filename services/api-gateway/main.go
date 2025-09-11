6c5b289a0dd96ae6059f54af7200c70a3e7d9581
Muhammad <155086759+muhammad21236@users.noreply.github.com>
2025-09-11 20:36:50 +0500

    Gracefull shutdowns'

--- services/api-gateway/main.go
+++ services/api-gateway/main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"ride-sharing/shared/env"
 )
 
 var (
-	httpAddr = env.GetString("HTTP_ADDR", ":8081")
+	httpAddr = env.GetString("HTTP_ADDR", ":8088")
 )
 
 func main() {
@@ -23,7 +28,28 @@ func main() {
 		Handler: mux,
 	}
 
-	if err := server.ListenAndServe(); err != nil {
-		log.Fatalf("Failed to start server: %v", err)
+	serverErrors := make(chan error, 1)
+
+	go func() {
+		log.Printf("Listening on %s", httpAddr)
+		serverErrors <- server.ListenAndServe()
+	}()
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+
+	select {
+	case err := <-serverErrors:
+		log.Fatalf("Error starting server: %v", err)
+	case sig := <-shutdown:
+		log.Printf("Received %v, initiating shutdown", sig)
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		if err := server.Shutdown(ctx); err != nil {
+			log.Printf("Error during server shutdown: %v", err)
+			server.Close()
+		}
 	}
+
+	log.Println("Server gracefully stopped")
 }
--- services/trip-service/cmd/main.go
+++ services/trip-service/cmd/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	h "ride-sharing/services/trip-service/internal/infrastructure/http"
 	"ride-sharing/services/trip-service/internal/infrastructure/repository"
 	"ride-sharing/services/trip-service/internal/service"
+	"syscall"
+	"time"
 )
 
 func main() {
@@ -24,8 +29,27 @@ func main() {
 		Handler: mux,
 	}
 
-	if err := server.ListenAndServe(); err != nil {
-		log.Printf("HTTP server error: %v", err)
+	serverErrors := make(chan error, 1)
+
+	go func() {
+		log.Printf("Listening on %s", ":8083")
+		serverErrors <- server.ListenAndServe()
+	}()
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+
+	select {
+	case err := <-serverErrors:
+		log.Fatalf("Error starting server: %v", err)
+	case sig := <-shutdown:
+		log.Printf("Received %v, initiating shutdown", sig)
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		if err := server.Shutdown(ctx); err != nil {
+			log.Printf("Error during server shutdown: %v", err)
+			server.Close()
+		}
 	}
 
 }
